b49c181e3cacf1466442c6be6180e7f915629874
ABHIRAM YERRAMSETTI <abhiram.yerramsetty@gmail.com>
2024-07-06 16:23:34 +0530

    Update dep.py

--- dep.py
+++ dep.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error,r2_score
 from sklearn.linear_model import LinearRegression
